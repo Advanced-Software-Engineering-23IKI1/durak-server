c6687969ab442e84dd2751bc06e4f53e76746b13
Fabian <107812436+fabischw@users.noreply.github.com>
2026-03-09 19:41:24 +0100

    small readibility changes

--- src/durak_server/packages/player_status_package.py
+++ src/durak_server/packages/player_status_package.py
@@ -2,18 +2,17 @@ from durak_server.packages import BasePackage
 from durak_server.player import PlayerGameStatus
 from typing import Union
 
+
 class PlayerStatusPackage(BasePackage):
     PACKAGE_TYPE = "player-status"
-    JSON_PARAM_MAP = {
-        "statuses": "statuses"
-    }
+    JSON_PARAM_MAP = {"statuses": "statuses"}
 
     def __init__(self, statuses: list[dict[str, Union[int, str]]]):
         """PlayerStatusPackage
         see the package documentation for more information
 
         Args:
-            statuses (list[dict[str, Union[int, str]]]): list of players with their readiness status
+            statuses (list[dict[str, Union[int, str]]]): list of dicts with player_id and status indicating their current role
 
         Raises:
             ValueError: on invalid player list
@@ -27,29 +26,35 @@ class PlayerStatusPackage(BasePackage):
         More information on the required structure and data can be found in the package documentation
 
         Args:
-            statuses (list[dict]): input player status list
+            statuses (list[dict[str, Union[int, str]]]): list of player status dicts
 
         Returns:
             bool: flag
         """
         keys = {"player_id", "status"}
-        if not all(set(player.keys()) == keys for player in statuses):  # structure check
+        if not all(
+            set(player.keys()) == keys for player in statuses
+        ):  # structure check
             return False
-        return all((isinstance(player["status"], PlayerGameStatus) or (player["status"] in [state.value for state in PlayerGameStatus])) for player in statuses)
+        return all(
+            (
+                isinstance(player["status"], PlayerGameStatus)
+                or (player["status"] in [state.value for state in PlayerGameStatus])
+            )
+            for player in statuses
+        )  # dtype check
 
     def _generate_body_dict(self) -> dict:
         # convert Enum into string representation if necessary
-        for player in self.__statuses:
-            if isinstance(player["status"], PlayerGameStatus):
-                player["status"] = player["status"].value
+        for player_status_dict in self.__statuses:
+            if isinstance(player_status_dict["status"], PlayerGameStatus):
+                player_status_dict["status"] = player_status_dict["status"].value
 
-        dict_repr = {
-            "statuses": self.__statuses
-        }
+        dict_repr = {"statuses": self.__statuses}
         return dict_repr
 
     @property
-    def statuses(self) -> list[dict[str, Union[int, bool]]]:
+    def statuses(self) -> list[dict[str, Union[int, str]]]:
         return self.__statuses
 
     def __repr__(self):
