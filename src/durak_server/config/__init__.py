dabebbb7edbdfab879130515e7f9f728d81de8aa
d-e-a-d-i-n-s-i-d-e <122444596+d-e-a-d-i-n-s-i-d-e@users.noreply.github.com>
2026-03-27 12:34:31 +0100

    add more config options

--- src/durak_server/config/__init__.py
+++ src/durak_server/config/__init__.py
@@ -1,26 +1,32 @@
 from durak_server.config.game_config import BasicGameConfig, GameConfig
 from durak_server.game.card import CardValue, Deck52
 from durak_server.config.card_distribution import CARDCOUNT_MAPPING
+from durak_server import CONFIG
 
 default_game_config = BasicGameConfig(
-    attack_forwarding=True,
-    attack_forwarding_exact_count_match=False,
-    all_card_defend_early_end=False,
-    card_order=[
-        CardValue.TWO,
-        CardValue.THREE,
-        CardValue.FOUR,
-        CardValue.FIVE,
-        CardValue.SIX,
-        CardValue.SEVEN,
-        CardValue.EIGHT,
-        CardValue.NINE,
-        CardValue.TEN,
-        CardValue.J,
-        CardValue.Q,
-        CardValue.K,
-        CardValue.A,
-    ],
-    player_card_count = 7,
-    deck = Deck52()
+    attack_forwarding=bool(CONFIG.get("game", "ATTACK_FORWARDING")),
+    attack_forwarding_exact_count_match=bool(CONFIG.get("game", "EXACT_COUNT_MATCH")),
+    all_card_defend_early_end=bool(CONFIG.get("game", "DEFEND_EARLY_END")),
+    card_order=list(CONFIG.get("game", "CARD_ORDER")),
+
+    player_card_count=int(CONFIG.get("game", "PLAYER_CARD_COUNT")),
+    deck= {"52": Deck52()}[CONFIG.get("game", "DECK_TYPE")]
+    
+#    card_order=[
+#        CardValue.TWO,
+#        CardValue.THREE,
+#        CardValue.FOUR,
+#        CardValue.FIVE,
+#        CardValue.SIX,
+#        CardValue.SEVEN,
+#        CardValue.EIGHT,
+#        CardValue.NINE,
+#        CardValue.TEN,
+#        CardValue.J,
+#        CardValue.Q,
+#        CardValue.K,
+#        CardValue.A,
+#    ],
+#    player_card_count = 7,
+#    deck = Deck52()
 )
